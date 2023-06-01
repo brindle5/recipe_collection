f79a1631dd48db1dfbd1ef9a51f1c908277aa020
brindle5 <>
2023-06-01 17:14:01 +0000

    Update classes in views.py

--- recipebook/views.py
+++ recipebook/views.py
@@ -1,14 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Cuisine, Meal, Recipe
 from django.http import HttpResponse
 from django.views import generic, View
 
 
-def display_recipe(request):
+# def display_recipe(request):
 
-    recipe = Recipe.objects.all().values
+#     return render(request, 'recipe.html')
 
-    context = {
-        'recipe': recipe
-    }
-    return render(request, context, 'recipe.html')
+class RecipeCollection(generic.ListView):
+    queryset = Recipe.objects.values_list('title')
+    template_name = 'recipe.html'
+    paginate_by = 9
