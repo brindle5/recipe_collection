be3286b57a28826d0eef230c51f9177516ac646c
brindle5 <>
2023-06-04 14:58:15 +0000

    Add css file to static folder.

--- recipebook/urls.py
+++ recipebook/urls.py
@@ -1,8 +1,7 @@
 from . import views
 from django.urls import path
-from recipebook import views
+
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
     path('display', views.display_recipe, name="recipe"),
 ]
