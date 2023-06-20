b6d1190cf9dfbe964319a1d51312d43f02b35cf1
brindle5 <sarah.brindle@hotmail.co.uk>
2023-06-20 10:17:11 +0000

    Add and run test views.

--- recipebook/test_forms.py
+++ recipebook/test_forms.py
@@ -1,40 +1,42 @@
-from django.test import TestCase
-from .forms import RecipeForm
+# from django.test import TestCase
+# from .forms import RecipeForm
 
-class TestRecipeForm(TestCase):
+# Tests based on the Code Institute ToDo App project view tests
 
-    def test_recipe_title_is_required(self):
-        form = RecipeForm({'title': ''})
-        self.assertFalse(form.is_valid())
+# class TestRecipeForm(TestCase):
 
-    def test_description_title_is_required(self):
-        form = RecipeForm({'description': ''})
-        self.assertFalse(form.is_valid())
+#     def test_recipe_title_is_required(self):
+#         form = RecipeForm({'title': ''})
+#         self.assertFalse(form.is_valid())
 
-    def test_description_is_required(self):
-        form = RecipeForm({'description': ''})
-        self.assertFalse(form.is_valid())
+#     def test_description_title_is_required(self):
+#         form = RecipeForm({'description': ''})
+#         self.assertFalse(form.is_valid())
 
-    def test_ingredients_are_required(self):
-        form = RecipeForm({'ingredients': ''})
-        self.assertFalse(form.is_valid())
+#     def test_description_is_required(self):
+#         form = RecipeForm({'description': ''})
+#         self.assertFalse(form.is_valid())
 
-    def test_cuisine_is_required(self):
-        form = RecipeForm({'cuisine': ''})
-        self.assertFalse(form.is_valid())
+#     def test_ingredients_are_required(self):
+#         form = RecipeForm({'ingredients': ''})
+#         self.assertFalse(form.is_valid())
 
-    def test_meal_is_required(self):
-        form = RecipeForm({'meal': ''})
-        self.assertFalse(form.is_valid())
+#     def test_cuisine_is_required(self):
+#         form = RecipeForm({'cuisine': ''})
+#         self.assertFalse(form.is_valid())
 
-    def test_form_names_are_explicit(self):
-        form = RecipeForm()
-        self.assertEqual(form.Meta.fields,[
-            'title', 
-            'author', 
-            'description', 
-            'ingredients',
-            'instructions',
-            'cuisine',
-            'meal'
-            ])
+#     def test_meal_is_required(self):
+#         form = RecipeForm({'meal': ''})
+#         self.assertFalse(form.is_valid())
+
+#     def test_form_names_are_explicit(self):
+#         form = RecipeForm()
+#         self.assertEqual(form.Meta.fields,[
+#             'title', 
+#             'author', 
+#             'description', 
+#             'ingredients',
+#             'instructions',
+#             'cuisine',
+#             'meal'
+#             ])
--- recipebook/test_views.py
+++ recipebook/test_views.py
@@ -1,7 +1,39 @@
-# from django.test import TestCase
+from django.test import TestCase
+from . import views
+from .models import Cuisine, Meal, Recipe
 
-# # Create your tests here.
+# Tests based on the Code Institute ToDo App project view tests
 
-# class TestProject(TestCase):
+class TestViews(TestCase):
 
-#     def
+    def get_homepage(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+    def get_add_recipe_page(self):
+        response = self.client.get('/add')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'add_recipe.html')
+    
+    def get_edit_recipe_page(self):
+        recipe = Recipe.objects.create(name="Test Recipe")
+        response = self.client.get(f'/edit/{recipe.id}')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'edit_recipe.html')
+
+    def can_add_recipe(self):
+        response = self.client.post('/add', {'name': 'Test Add Recipe'})
+        self.assertRedirects(response='/')
+
+    def can_edit_recipe(self):
+        response = self.client.post('/edit', {'name': 'Test Edit Recipe'})
+        self.assertRedirects(response='/')
+
+    def can_delete_recipe(self):
+        recipe = Recipe.objects.create(name="Test Recipe")
+        response = self.client.get(f'/delete/{recipe.id}')
+        self.assertRedirects(response='/')
+        existing_items = Recipe.objects.filter(id=item.id)
+        self.assertEqual(len(existing_items), 0)
+    
