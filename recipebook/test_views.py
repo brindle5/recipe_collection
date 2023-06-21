068591affbbcaa89cb7e16e8c8e1a836230bd86b
brindle5 <sarah.brindle@hotmail.co.uk>
2023-06-21 09:51:48 +0000

    Delete non-functioning tests, and comment out non-working button code.

--- recipebook/test_views.py
+++ recipebook/test_views.py
@@ -14,24 +14,5 @@ class TestViews(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'add_recipe.html')
     
-    # def test_get_edit_recipe_page(self):
-    #     recipe = Recipe.objects.create(name="Test Recipe")
-    #     response = self.client.get(f'/edit/{recipe.id}')
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, 'edit_recipe.html')
 
-    # def test_can_add_recipe(self):
-    #     response = self.client.post('/add', {'name': 'Test Add Recipe'})
-    #     self.assertRedirects(response='/')
-
-    # def test_can_edit_recipe(self):
-    #     response = self.client.post('/edit', {'name': 'Test Edit Recipe'})
-    #     self.assertRedirects(response='/')
-
-    # def test_can_delete_recipe(self):
-    #     recipe = Recipe.objects.create(name="Test Recipe")
-    #     response = self.client.get(f'/delete/{recipe.id}')
-    #     self.assertRedirects(response='/')
-    #     existing_items = Recipe.objects.filter(id=item.id)
-    #     self.assertEqual(len(existing_items), 0)
     
