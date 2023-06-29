b6295130f3d8ca09c5c8b212104e84501a671eed
brindle5 <sarah.brindle@hotmail.co.uk>
2023-06-29 18:20:08 +0000

    Turn debug mode off

--- thekitchen/settings.py
+++ thekitchen/settings.py
@@ -33,7 +33,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-brindle5-recipe-collecti-wx491azm6d.us2.codeanyapp.com',
                  'localhost',
